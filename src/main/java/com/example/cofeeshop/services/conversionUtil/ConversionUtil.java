d92de39b63f302f955d66a052396841c9357e3b3
ehsan <sir.ehrp@gmail.com>
2021-05-30 22:25:55 +0430

    now using conversion utilities

--- src/main/java/com/example/cofeeshop/services/conversionUtil/ConversionUtil.java
+++ src/main/java/com/example/cofeeshop/services/conversionUtil/ConversionUtil.java
@@ -2,7 +2,11 @@ package com.example.cofeeshop.services.conversionUtil;
 
 import com.example.cofeeshop.domain.Menu;
 import com.example.cofeeshop.services.dto.MenuDTO;
+import com.example.cofeeshop.services.dto.MenuListDTO;
+
+import java.util.List;
 
 public interface ConversionUtil {
     MenuDTO menuToMenuDTO(Menu menu);
+    MenuListDTO listAllMenus(List<Menu> menus);
 }
--- src/main/java/com/example/cofeeshop/services/conversionUtil/ConversionUtilImpl.java
+++ src/main/java/com/example/cofeeshop/services/conversionUtil/ConversionUtilImpl.java
@@ -1,15 +1,15 @@
 package com.example.cofeeshop.services.conversionUtil;
 
 import com.example.cofeeshop.domain.Menu;
-import com.example.cofeeshop.services.dto.*;
 import com.example.cofeeshop.services.conversionUtil.mappers.CategoryMapper;
 import com.example.cofeeshop.services.conversionUtil.mappers.FoodMapper;
 import com.example.cofeeshop.services.conversionUtil.mappers.MenuMapper;
+import com.example.cofeeshop.services.dto.MenuDTO;
+import com.example.cofeeshop.services.dto.MenuListDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -22,17 +22,15 @@ public class ConversionUtilImpl implements ConversionUtil {
     private final FoodMapper foodMapper;
     private final FoodUtil foodUtil;
     private final CategoryUtil categoryUtil;
+    private final MenuUtil menuUtil;
 
     @Override
     public MenuDTO menuToMenuDTO(Menu menu) {
-        var menuDTO = menuMapper.menuToMenuDTO(menu);
+        return menuUtil.menuToMenuDTO(menu);
+    }
 
-        var categoryList = menu.getCategories();
-
-        var categoryDTOList = categoryUtil.listAllCategories(categoryList);
-
-        menuDTO.setCategoryListDTO(categoryDTOList);
-
-        return menuDTO;
+    @Override
+    public MenuListDTO listAllMenus(List<Menu> menus) {
+        return menuUtil.listAllMenus(menus);
     }
 }
