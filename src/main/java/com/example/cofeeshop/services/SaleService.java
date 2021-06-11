5e43a24bff3ce5577a64815f7b318fb0dc3c1ff2
ehsan <sir.ehrp@gmail.com>
2021-06-11 16:58:41 +0430

    created sale service interface

---
+++ src/main/java/com/example/cofeeshop/services/SaleService.java
@@ -0,0 +1,15 @@
+package com.example.cofeeshop.services;
+
+import com.example.cofeeshop.services.dto.SaleDTO;
+import com.example.cofeeshop.services.dto.SaleListDTO;
+
+public interface SaleService {
+
+    SaleListDTO findAllSales();
+
+    SaleDTO findSaleById(Long id);
+
+    SaleDTO editSale(Long saleId,SaleDTO saleDTO);
+
+    SaleDTO createSale(Long foodId, Integer quantity);
+}
